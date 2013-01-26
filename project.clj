0fdcfaccc71a40235603cbe3dccc5b1547ae84e5
Jostein Kjønigsen <jostein@kjonigsen.net>
2013-01-26 13:02:16 +0100

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-drip "0.1.0-SNAPSHOT"
+(defproject lein-drip "0.1.1-SNAPSHOT"
   :description "Download, bootstrap and register drip as the default JVM for leiningen."
   :url "https://github.com/josteink/lein-drip"
   :license {:name "Eclipse Public License"
